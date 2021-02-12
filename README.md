057ec97f6c8f034c62af9f0a9c882fbd67c4de7f
singleye <singleye@163.com>
2021-02-12 12:31:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# OS requirement
+
+Install [Ubuntu Server](https://ubuntu.com/download/raspberry-pi) on raspberry pi.
+
+# Tools
+
+* install.sh: install ROS2 on raspberry pi
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+sudo apt update
+sudo apt install ros-foxy-ros-base
+sudo apt install python3-colcon-common-extensions
+sudo apt install python3-argcomplete
+
+echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
